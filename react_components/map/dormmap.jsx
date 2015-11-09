574765f8a0934195cd7eb54d5ab507aceb4a4d1e
phiaseitz <sophia.seitz@students.olin.edu>
2015-11-08 22:53:38 -0500

    Allow switching rooms

--- react_components/map/dormmap.jsx
+++ react_components/map/dormmap.jsx
@@ -19,6 +19,7 @@ var DormMap = React.createClass({
   },
   render: function() {
     var props = this.props;
+    console.log(this.props);
 
     var disagreeColor = "#FF9933";
     var middleColor = "#FFFFFF";
