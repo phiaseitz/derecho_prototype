aa1c997aacc6afd578301df2b5ac9a4692eea65d
tchoetso <tchoetso920@gmail.com>
2015-11-09 12:22:56 -0500

    changes in login page css

--- react_components/preview_card/preview_card.jsx
+++ react_components/preview_card/preview_card.jsx
@@ -32,7 +32,7 @@ var PreviewCard = React.createClass({
     var confirmationTextShown =
         this.state.confirmationInProgress ? "visible" : "hidden";
     var roomNumber = (this.props.previewPin.hall ? 'East Hall' : 'West Hall') +
-        ' ' + this.props.previewPin.roomNumber;
+        ' ' + this.props.previewPin.room;
 
     var occupantInfo = [];
     for (i = 0; i < this.props.previewPin.occupants.length; i++) {
@@ -88,6 +88,9 @@ var PreviewCard = React.createClass({
             setPin={this.props.setPin}
             setConfirm={this.setConfirmationInProgress}
             inProgress={this.state.confirmationInProgress}
+            userPin = {this.props.userPin}
+            cardHall = {this.props.previewPin.hall}
+            cardRoom = {this.props.previewPin.room}
           />
         </div>
       </div>
