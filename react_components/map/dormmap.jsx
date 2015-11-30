57d385fd608416148612b97837ac2e0bbf9351d4
phiaseitz <sophia.seitz@students.olin.edu>
2015-11-30 17:27:29 -0500

    Much lighter colors

--- react_components/map/dormmap.jsx
+++ react_components/map/dormmap.jsx
@@ -27,9 +27,9 @@ var DormMap = React.createClass({
     var props = this.props;
     console.log(this.props);
 
-    var disagreeColor = "#ff4d4d";
+    var disagreeColor = "#ff8080";
     var middleColor = "#ddccff";
-    var agreeColor = "#0099ff";
+    var agreeColor = "#66c2ff";
 
     var maxDormWidth = props.width - 1.3*props.margin;
     var maxDormHeight = props.height - 1.1*props.margin;
