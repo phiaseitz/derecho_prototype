07e56f69ecfbe7719302469f7ebc8358c7c3c9b1
Ian Hill <ian.hill@students.olin.edu>
2015-12-06 20:30:29 -0500

    Fix pinning flow and allow for multiple pins

--- react_components/mocks/pin.jsx
+++ react_components/mocks/pin.jsx
@@ -1,29 +1,31 @@
-var currentUserPinData =  {
-  group: "A",
-  occupants: [
-    {
-    firstName: "Sophia",
-    lastName: "Seitz",
-    contactMethods: ["Email", "Text"],
-    contactValues: [
-      "sophia.seitz@students.olin.edu",
-      "555-555-5555"
-    ]
-  },
+var currentUserPinData = [
   {
-    firstName: "Tenzin",
-    lastName: "Choetso",
-    contactMethods: ["Email", "Text"],
-    contactValues: [
-      "tenzin.choetso@students.olin.edu",
-      "555-555-5556"
-    ]
+    group: "A",
+    occupants: [
+      {
+      firstName: "Sophia",
+      lastName: "Seitz",
+      contactMethods: ["Email", "Text"],
+      contactValues: [
+        "sophia.seitz@students.olin.edu",
+        "555-555-5555"
+      ]
+    },
+    {
+      firstName: "Tenzin",
+      lastName: "Choetso",
+      contactMethods: ["Email", "Text"],
+      contactValues: [
+        "tenzin.choetso@students.olin.edu",
+        "555-555-5556"
+      ]
+    }
+    ],
+    hall: 1,
+    room: 409,
+    tags: [{label:"Partying", value: 0},
+           {label: "Movies", value: 3}]
   }
-  ],
-  hall: 1,
-  room: 409,
-  tags: [{label:"Partying", value: 0},
-         {label: "Movies", value: 3}]
-};
+];
 
 module.exports = currentUserPinData;
