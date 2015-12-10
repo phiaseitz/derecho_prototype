7e8466c24b405cf85145b4a49cfd2808730bff2d
jagreene <austin.greene@students.olin.edu>
2015-12-09 20:51:50 -0500

    updated zero prefix

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -33289,10 +33289,10 @@ module.exports = PinData;
 
 },{}],181:[function(require,module,exports){
 var sampleTags = [
-  {label: "Tea", value: 9},
+  {label: "Tea", value: 1},
   {label: "Coffee", value: 6},
   {label: "Rage Cage", value: 7},
-  {label: "Loud", value: 1},
+  {label: "Loud", value: 9},
   {label: "Studying", value: 4},
   {label: "Partying", value: 2},
   {label: "Movies", value : 1},
