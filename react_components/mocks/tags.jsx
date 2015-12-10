fadca843a2603762465ae87034d3b1f72130ff06
jagreene <austin.greene@students.olin.edu>
2015-12-09 20:49:36 -0500

    updated zero prefix

--- react_components/mocks/tags.jsx
+++ react_components/mocks/tags.jsx
@@ -1,8 +1,8 @@
 var sampleTags = [
-  {label: "Tea", value: 9},
+  {label: "Tea", value: 1},
   {label: "Coffee", value: 6},
   {label: "Rage Cage", value: 7},
-  {label: "Loud", value: 1},
+  {label: "Loud", value: 9},
   {label: "Studying", value: 4},
   {label: "Partying", value: 2},
   {label: "Movies", value : 1},
